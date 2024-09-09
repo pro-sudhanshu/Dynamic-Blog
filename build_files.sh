8fd9e6bab195745786cac921463998b55c4f548a
pro-sudhanshu <codsmasterg@gmail.com>
2024-09-09 19:26:47 +0530

    some changes

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,3 @@ echo "BUILD END"
 echo "MAKE MIGRATIONS"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "COLLECT STATIC"
-python3.9 manage.py collectstatic --noinput --clear
