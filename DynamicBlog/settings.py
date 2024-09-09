d4c84273c512e4f591710979ec50f7bc0417d5cd
pro-sudhanshu <codsmasterg@gmail.com>
2024-09-09 19:05:32 +0530

    done vercel settings

--- DynamicBlog/settings.py
+++ DynamicBlog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w8094xqgi2&^n5*3!-l86qr#9iqg0a$6s!n0)r#(-q3+p^om+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ build_files.sh
@@ -0,0 +1,10 @@
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+echo "BUILD END"
+
+echo "MAKE MIGRATIONS"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "COLLECT STATIC"
+python3.9 manage.py collectstatic --noinput --clear
